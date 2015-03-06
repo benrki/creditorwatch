9926d6041c5d11049c9d96e787ac842e1cffcbb0
benrki <benjaminrki@gmail.com>
2015-03-06 23:50:31 +1100

    Add challenge1 functionality

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   get 'index/view'
 
+  get '/index' => 'index#view'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
